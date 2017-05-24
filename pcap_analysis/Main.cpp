67a617b01ed98f61393970500111b6006ac8ba14
Ajaay Chandrasekaran <ajaay@umich.edu>
2017-05-24 17:03:36 -0400

    A new program. But pcap.h not found

--- pcap_analysis/Main.cpp
+++ pcap_analysis/Main.cpp
@@ -1,183 +1,94 @@
+/*Code obtained from:
+www.rhyous.com/2011/11/13/how-to-read-a-pcap-file-from-wireshark-with-c
+*/
+
 /* Demonstration program of reading packet trace files recorded by pcap
 * (used by tshark and tcpdump) and dumping out some corresponding information
 * in a human-readable form.
 *
-* Note, this program is limited to processing trace files that contains
-* UDP packets.  It prints the timestamp, source port, destination port,
-* and length of each such packet.
+
+/*
+* Step 1 - Add includes
 */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <net/if.h>
-#include <netinet/if_ether.h>
-
+#include <string>
+#include <iostream>
 #include <pcap.h>
 
-
-/* We've included the UDP header struct for your ease of customization.
-* For your protocol, you might want to look at netinet/tcp.h for hints
-* on how to deal with single bits or fields that are smaller than a byte
-* in length.
-*
-* Per RFC 768, September, 1981.
-*/
-struct UDP_hdr {
-	u_short	uh_sport;		/* source port */
-	u_short	uh_dport;		/* destination port */
-	u_short	uh_ulen;		/* datagram length */
-	u_short	uh_sum;			/* datagram checksum */
-};
-
-
-/* Some helper functions, which we define at the end of this file. */
-
-/* Returns a string representation of a timestamp. */
-const char *timestamp_string(struct timeval ts);
-
-/* Report a problem with dumping the packet with the given timestamp. */
-void problem_pkt(struct timeval ts, const char *reason);
-
-/* Report the specific problem of a packet being too short. */
-void too_short(struct timeval ts, const char *truncated_hdr);
-
-/* dump_UDP_packet()
-*
-* This routine parses a packet, expecting Ethernet, IP, and UDP headers.
-* It extracts the UDP source and destination port numbers along with the UDP
-* packet length by casting structs over a pointer that we move through
-* the packet.  We can do this sort of casting safely because libpcap
-* guarantees that the pointer will be aligned.
-*
-* The "ts" argument is the timestamp associated with the packet.
-*
-* Note that "capture_len" is the length of the packet *as captured by the
-* tracing program*, and thus might be less than the full length of the
-* packet.  However, the packet pointer only holds that much data, so
-* we have to be careful not to read beyond it.
-*/
-void dump_UDP_packet(const unsigned char *packet, struct timeval ts,
-	unsigned int capture_len)
-{
-	struct ip *ip;
-	struct UDP_hdr *udp;
-	unsigned int IP_header_length;
-
-	/* For simplicity, we assume Ethernet encapsulation. */
-
-	if (capture_len < sizeof(struct ether_header))
-	{
-		/* We didn't even capture a full Ethernet header, so we
-		* can't analyze this any further.
-		*/
-		too_short(ts, "Ethernet header");
-		return;
-	}
-
-	/* Skip over the Ethernet header. */
-	packet += sizeof(struct ether_header);
-	capture_len -= sizeof(struct ether_header);
-
-	if (capture_len < sizeof(struct ip))
-	{ /* Didn't capture a full IP header */
-		too_short(ts, "IP header");
-		return;
-	}
-
-	ip = (struct ip*) packet;
-	IP_header_length = ip->ip_hl * 4;	/* ip_hl is in 4-byte words */
-
-	if (capture_len < IP_header_length)
-	{ /* didn't capture the full IP header including options */
-		too_short(ts, "IP header with options");
-		return;
-	}
-
-	if (ip->ip_p != IPPROTO_UDP)
-	{
-		problem_pkt(ts, "non-UDP packet");
-		return;
-	}
-
-	/* Skip over the IP header to get to the UDP header. */
-	packet += IP_header_length;
-	capture_len -= IP_header_length;
-
-	if (capture_len < sizeof(struct UDP_hdr))
-	{
-		too_short(ts, "UDP header");
-		return;
-	}
-
-	udp = (struct UDP_hdr*) packet;
-
-	printf("%s UDP src_port=%d dst_port=%d length=%d\n",
-		timestamp_string(ts),
-		ntohs(udp->uh_sport),
-		ntohs(udp->uh_dport),
-		ntohs(udp->uh_ulen));
-}
-
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	pcap_t *pcap;
-	const unsigned char *packet;
-	char errbuf[PCAP_ERRBUF_SIZE];
-	struct pcap_pkthdr header;
-
-	/* Skip over the program name. */
-	++argv; --argc;
-
-	/* We expect exactly one argument, the name of the file to dump. */
-	if (argc != 1)
-	{
-		fprintf(stderr, "program requires one argument, the trace file to dump\n");
-		exit(1);
-	}
-
-	pcap = pcap_open_offline(argv[0], errbuf);
-	if (pcap == NULL)
-	{
-		fprintf(stderr, "error reading pcap file: %s\n", errbuf);
-		exit(1);
-	}
-
-	/* Now just loop through extracting packets as long as we have
-	* some to read.
+	/*
+	* Step 2 - Get a file name
 	*/
-	while ((packet = pcap_next(pcap, &header)) != NULL)
-		dump_UDP_packet(packet, header.ts, header.caplen);
 
-	// terminate
-	return 0;
-}
+	string file = "C:\\users\\jared\\testfiles\\smallcapture.pcap";
 
+	/*
+	* Step 3 - Create an char array to hold the error.
+	*/
 
-/* Note, this routine returns a pointer into a static buffer, and
-* so each call overwrites the value returned by the previous call.
-*/
-const char *timestamp_string(struct timeval ts)
-{
-	static char timestamp_string_buf[256];
+	// Note: errbuf in pcap_open functions is assumed to be able to hold at least PCAP_ERRBUF_SIZE chars
+	//       PCAP_ERRBUF_SIZE is defined as 256.
+	// http://www.winpcap.org/docs/docs_40_2/html/group__wpcap__def.html
+	char errbuff[PCAP_ERRBUF_SIZE];
 
-	sprintf(timestamp_string_buf, "%d.%06d",
-		(int)ts.tv_sec, (int)ts.tv_usec);
+	/*
+	* Step 4 - Open the file and store result in pointer to pcap_t
+	*/
 
-	return timestamp_string_buf;
-}
+	// Use pcap_open_offline
+	// http://www.winpcap.org/docs/docs_41b5/html/group__wpcapfunc.html#g91078168a13de8848df2b7b83d1f5b69
+	pcap_t * pcap = pcap_open_offline(file.c_str(), errbuff);
 
-void problem_pkt(struct timeval ts, const char *reason)
-{
-	fprintf(stderr, "%s: %s\n", timestamp_string(ts), reason);
-}
+	/*
+	* Step 5 - Create a header and a data object
+	*/
 
-void too_short(struct timeval ts, const char *truncated_hdr)
-{
-	fprintf(stderr, "packet with timestamp %s is truncated and lacks a full %s\n",
-		timestamp_string(ts), truncated_hdr);
+	// Create a header object:
+	// http://www.winpcap.org/docs/docs_40_2/html/structpcap__pkthdr.html
+	struct pcap_pkthdr *header;
+
+	// Create a character array using a u_char
+	// u_char is defined here:
+	// C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Include\WinSock2.h
+	// typedef unsigned char   u_char;
+	const u_char *data;
+
+	/*
+	* Step 6 - Loop through packets and print them to screen
+	*/
+	u_int packetCount = 0;
+	while (int returnValue = pcap_next_ex(pcap, &header, &data) >= 0)
+	{
+		// Print using printf. See printf reference:
+		// http://www.cplusplus.com/reference/clibrary/cstdio/printf/
+
+		// Show the packet number
+		printf("Packet # %i\n", ++packetCount);
+
+		// Show the size in bytes of the packet
+		printf("Packet size: %d bytes\n", header->len);
+
+		// Show a warning if the length captured is different
+		if (header->len != header->caplen)
+			printf("Warning! Capture size different than packet size: %ld bytes\n", header->len);
+
+		// Show Epoch Time
+		printf("Epoch Time: %d:%d seconds\n", header->ts.tv_sec, header->ts.tv_usec);
+
+		// loop through the packet and print it as hexidecimal representations of octets
+		// We also have a function that does this similarly below: PrintData()
+		for (u_int i = 0; (i < header->caplen); i++)
+		{
+			// Start printing on the next after every 16 octets
+			if ((i % 16) == 0) printf("\n");
+
+			// Print each octet as hex (x), make sure there is always two characters (.2).
+			printf("%.2x ", data[i]);
+		}
+
+		// Add two lines between packets
+		printf("\n\n");
+	}
 }
