9e86a44ccdd30feb28bbf5299657b5d26d3e8307
Ajaay Chandrasekaran <ajaay@umich.edu>
2017-05-30 14:09:07 -0400

    Begin extracting calibration data

--- pcap_analysis/calibrationTableOutput.h
+++ pcap_analysis/calibrationTableOutput.h
@@ -7,6 +7,11 @@
 #include "packetOutput.h"
 #include "definitions.h"
 
+struct l_status{
+	u_char type;
+	u_char value;
+};
+
 //Class that stores calibration data by reading packets one after another.
 class calibrationTableOutput : public packetOutput {
 private:
@@ -16,23 +21,42 @@ private:
 	laser_params params[NUM_LASERS];
 
 	//This function ignores packets until it sees the 'U','N','I','T'
-	//as status values 
-	u_char currCycle[CYCLE_LENGTH];
+	//as status values
+	l_status currCycle[CYCLE_LENGTH];
+
+	//TODO: resolve compiler issues. Make static
+	u_char STATUS_TYPES[CYCLE_LENGTH];
+
+	//Current index of status_types array. Helps check for packet continuity in
+	//case packets were missed
+	int currIndex;
 
 	bool detectedUNIT;
 public:
 	calibrationTableOutput(string outFile) : packetOutput(outFile){
 		detectedUNIT = false;
+		//Workaround initialization
+		u_char temp[] = { 'H', 'M', 'S', 'D', 'N', 'Y', 'G', 'T', 'V', '1', '2', '3', '4', '5', '6', '7' };
+		memcpy(temp, STATUS_TYPES, CYCLE_LENGTH);
+
+		currIndex = 0;
+
 		memset((void*)params,0,sizeof(laser_params) * NUM_LASERS);
+		memset((void*)currCycle,0,sizeof(l_status) * CYCLE_LENGTH);
 	}
 
-	//Takes in a pointer to beginning of the packet. Requires
-	//that packptr points to memory allocated at 1248 bytes.
-	//Also requires that this function is called on consecutive
+	//Requires that this function is called on consecutive
 	//packets. (The caller must give every packet that it receives
 	//to this function without skipping)
-	void recordNewPacket(const lidarPacket* packptr){
+	void recordNewPacket(const lidarPacket& packptr){
+		if (!detectedUNIT) {
+			if (packptr.statusType == '1' && packptr.statusVal == 'U'){
+				detectedUNIT = true;
+			}
+		}
+		else {
 
+		}
 	}
 
 
--- pcap_analysis/laserOutput.h
+++ pcap_analysis/laserOutput.h
@@ -10,18 +10,6 @@ private:
 
 public:
 	laserOutput(string outFile) : packetOutput(outFile) {
-		fprintf(pktFile, "Packet Data\n");
-
-		fprintf(pktFile,
-			"%s%s%s%s%s%s%s%s",
-			"GPS Timestamp,",
-			"Timestamp in us,",
-			"Status Type,",
-			"Status Type Decimal,",
-			"Status Type ASCII,",
-			"Status Value,",
-			"Status Value Decimal,",
-			"Status Value ASCII\n");
 	}
 
 	//Takes in a pointer to beginning of the packet. Requires
