380a6c53bfa4c0976459e482c7185eeae6e78d6f
Ajaay Chandrasekaran <ajaay@umich.edu>
2017-06-07 12:32:54 -0400

    Laser output cleaning

--- pcap_analysis/laserOutput.h
+++ pcap_analysis/laserOutput.h
@@ -15,22 +15,22 @@ public:
 	//Takes in a pointer to beginning of the packet. Requires
 	//that packptr points to memory allocated at 1248 bytes
 	void printLaserData(const lidarPacket& packptr){
-		fprintf(pktFile, "--------------------------------\n");
-		fprintf(pktFile, "PacketID,%d\n",packetID++);
+		fprintf(pktFile, "PacketID,%d\n", packetID++);
+		fprintf(pktFile, "-------------\n");
 		for (int i = 0; i < BLOCKS_PER_PACKET; ++i){
 			const laser_block& curr_block = packptr.blocks[i];
 
 			fprintf(pktFile, "Laser Block ID,%.4X\n", curr_block.laser_block_id);
 			fprintf(pktFile, "Rotational Position,%f\n", curr_block.rotational_pos / 100.0);
-
+			fprintf(pktFile, "Laser Index,cm Distance,Laser Intensity\n");
 			//Print all laser data for this block
-			for (int j = 0; j < LASERS_PER_BLOCK; ++j){
+			for(int j = 0; j < LASERS_PER_BLOCK; ++j){
 				const laser_point& curr_laser = curr_block.laserData[j];
-				fprintf(pktFile, "Laser Index,%u\n", (curr_block.laser_block_id == 0xDDFF ? j : j + LASERS_PER_BLOCK));
-				fprintf(pktFile, "cm Distance,%f\n", curr_laser.distance * 2 / 10.0);
-				fprintf(pktFile, "Laser Intensity,%u\n", curr_laser.intensity);
+				fprintf(pktFile, "%u,", (curr_block.laser_block_id == 0xDDFF ? j : j + LASERS_PER_BLOCK));
+				fprintf(pktFile, "%f,", curr_laser.distance * 2 / 10.0);
+				fprintf(pktFile, "%u\n", curr_laser.intensity);
 			}
-			fprintf(pktFile, "%s", "\n");
+			fprintf(pktFile, "--------------------------\n");
 		}
 	}
 
